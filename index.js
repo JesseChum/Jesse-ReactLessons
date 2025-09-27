265f038fd3a1e8f96adc7523caa7971489c97624
JesseChum <jessechum@icloud.com>
2025-09-26 21:47:07 -0700

    fixing import issue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client"
 import Header from "./Scrimba-Lessons/Header"
 import MainContent from "./MainContent"
-import Footer from "./Footer"
+import Footer from "./Scrimba-Lessons/Footer"
 const root = createRoot(document.getElementById("root"))
 
 //NOTE THIS IS THE FIRST TO USE INDEX.CSS FILE YOU CREATED
