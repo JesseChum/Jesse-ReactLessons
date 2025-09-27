7fd22c23e4123d5eb794319c0279510c3d58e67d
JesseChum <jessechum@icloud.com>
2025-09-26 19:25:20 -0700

    Created and added more content to files. Still completed challenges in order

---
+++ index.js
@@ -0,0 +1,27 @@
+import { createRoot } from "react-dom/client"
+import Header from "./Scrimba-Lessons/Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
+const root = createRoot(document.getElementById("root"))
+
+//NOTE THIS IS THE FIRST TO USE INDEX.CSS FILE YOU CREATED
+/**
+ * Challenge:
+ * 
+ * - Add a `nav` > `ul` > `li` (x3). The 3 items should say:
+ *   "Pricing", "About", and "Contact"
+ */
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
