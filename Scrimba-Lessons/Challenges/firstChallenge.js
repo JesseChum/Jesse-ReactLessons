6876306f7574fbcfdf3dcf3927cdafb7c9149247
JesseChum <jessechum@icloud.com>
2025-09-26 17:32:07 -0700

    Added challenger folder and completed first challenge

---
+++ Scrimba-Lessons/Challenges/firstChallenge.js
@@ -0,0 +1,11 @@
+  /**
+ * Challenge: Set up a React app from scratch again.
+ * This time, try rendering an unordered list with 2-3 list items inside
+ * with why you're excited to be learning React.
+ */
+import {createRoot} from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+root.render(<ul>
+<li> hello</li>
+<li> world</li>
+</ul>)
